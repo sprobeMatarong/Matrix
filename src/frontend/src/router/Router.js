92f50b9399c2a52bcebf122351621751768f1508
James Gordo <gordo.jt@sprobe.com>
2021-07-26 11:14:51 +0800

    Added Default 404 for Not Found Pages

--- src/frontend/src/router/Router.js
+++ src/frontend/src/router/Router.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Router as ReactRouter, Switch } from 'react-router-dom'
+import { Router as ReactRouter, Switch, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 
 import routes from './routes'
 import Public from './Public'
 import Private from './Private'
+import NotFound from '../views/NotFound'
 
 const history = createBrowserHistory()
 
@@ -19,6 +20,7 @@ function Router() {
 
           return <Public exact key={i} {...route} />
         })}
+        <Route component={NotFound} />
       </Switch>
     </ReactRouter>
   )
