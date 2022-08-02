ea9f6eb8065f40c6fe4535642fc3e653c8e814b1
James Gordo <gordo.jt@sprobe.com>
2022-08-02 15:46:15 +0800

    Added option to switch Yen symbol.

--- src/frontend/src/components/atoms/Formatter/JapanYen.js
+++ src/frontend/src/components/atoms/Formatter/JapanYen.js
@@ -1,20 +1,27 @@
 import PropTypes from 'prop-types';
 import BodyText from 'components/atoms/BodyText';
 
-function Yen({ amount }) {
+function Yen({ amount, isKanji }) {
   return (
     <BodyText>
-      {Number(amount).toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' })}
+      {!isKanji && '¥'}
+      {Number(amount).toLocaleString('ja-JP', {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      })}
+      {isKanji && '円'}
     </BodyText>
   );
 }
 
 Yen.defaultProps = {
   amount: 0,
+  isKanji: false,
 };
 
 Yen.propTypes = {
   amount: PropTypes.number.isRequired,
+  isKanji: PropTypes.bool,
 };
 
 export default Yen;
