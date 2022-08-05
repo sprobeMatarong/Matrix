8478fd5aa4be52146dc5f87b7746b54064b3ee0b
James Gordo <gordo.jt@sprobe.com>
2022-08-05 10:45:19 +0800

    Updated script of MacOS compatibility

--- setup.sh
+++ setup.sh
@@ -7,6 +7,20 @@ ORANGE_COLOR='\033[0;33m'
 NO_COLOR='\033[0m'
 BOLD_TEXT='\033[1m'
 
+function to_lower_case() {
+  echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
+# $1 = Pattern , $2 Filepath
+function set_env() {
+  # For MacOS
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    find $2 -type f -exec sed -i '' -e "$1" {} \;
+  else # For Other OS
+    sed -i "$1" $2
+  fi
+}
+
 if ! docker info > /dev/null 2>&1; then
   echo -e "${ORANGE_COLOR}[WARNING]${NO_COLOR} This script requires ${BOLD_TEXT}Docker Desktop${NO_COLOR} and it isn't running. Please start ${BOLD_TEXT}Docker Desktop${NO_COLOR} and try again!"
   exit 1
@@ -42,12 +56,12 @@ done
 # remove special characters and spaces and convert string to lowercase
 PROJECT_NAME=$(echo ${PROJECT_NAME//[^a-zA-Z ]/""})
 PROJECT_NAME=$(echo ${PROJECT_NAME//[ ]/"_"})
-PROJECT_NAME=$(echo ${PROJECT_NAME,,})
+PROJECT_NAME=$(to_lower_case $PROJECT_NAME)
 
 # Generate MySQL Credentials
-MYSQL_DATABASE="${PROJECT_NAME,,}"
-MYSQL_USER="${PROJECT_NAME,,}"
-MYSQL_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16})
+MYSQL_DATABASE=$(to_lower_case $PROJECT_NAME)
+MYSQL_USER=$(to_lower_case $PROJECT_NAME)
+MYSQL_PASSWORD=$(LC_ALL=C < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16})
 
 while [[ $APP_DOMAIN = "" ]]; do
    read -p 'Application Domain Name(e.g domain.com): ' APP_DOMAIN
@@ -60,18 +74,18 @@ if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
     # Update Docker .env
     echo -n "Setting up Docker environment variables ... "
     cp .env.example .env
-    sed -i "s/ENVIRONMENT=/ENVIRONMENT=$ENVIRONMENT/g" .env
-    sed -i "s/PROJECT_NAME=/PROJECT_NAME=$PROJECT_NAME/g" .env
-    sed -i "s/MYSQL_ROOT_PASSWORD=/MYSQL_ROOT_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16})/g" .env
-    sed -i "s/MYSQL_DATABASE=/MYSQL_DATABASE=$MYSQL_DATABASE/g" .env
-    sed -i "s/MYSQL_USER=/MYSQL_USER=$MYSQL_USER/g" .env
-    sed -i "s/MYSQL_PASSWORD=/MYSQL_PASSWORD=$MYSQL_PASSWORD/g" .env
+    set_env "s/ENVIRONMENT=/ENVIRONMENT=$ENVIRONMENT/g" .env;
+    set_env "s/PROJECT_NAME=/PROJECT_NAME=$PROJECT_NAME/g" .env;
+    set_env "s/MYSQL_ROOT_PASSWORD=/MYSQL_ROOT_PASSWORD=$(LC_ALL=C < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16})/g"  .env;
+    set_env "s/MYSQL_DATABASE=/MYSQL_DATABASE=$MYSQL_DATABASE/g"  .env;
+    set_env "s/MYSQL_USER=/MYSQL_USER=$MYSQL_USER/g"  .env;
+    set_env "s/MYSQL_PASSWORD=/MYSQL_PASSWORD=$MYSQL_PASSWORD/g"  .env;
 
-    sed -i "s/API_DOMAIN=/API_DOMAIN=api.$APP_DOMAIN/g" .env
-    sed -i "s/APP_DOMAIN=/APP_DOMAIN=$APP_DOMAIN/g" .env
+    set_env "s/API_DOMAIN=/API_DOMAIN=api.$APP_DOMAIN/g" .env;
+    set_env "s/APP_DOMAIN=/APP_DOMAIN=$APP_DOMAIN/g" .env;
 
     if [[ $ENVIRONMENT == "development" ]]; then
-        sed -i "s/ENABLE_XDEBUG=0/ENABLE_XDEBUG=1/g" .env
+        set_env "s/ENABLE_XDEBUG=0/ENABLE_XDEBUG=1/g" .env;
     fi
 
     echo -e "${SUCCESS_COLOR}done${NO_COLOR}"
@@ -79,29 +93,27 @@ if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
     # Update Laravel .env
     echo -n "Setting up Laravel environment variables ... "
     cp src/backend/.env.example src/backend/.env
-    sed -i "s/APP_NAME=Laravel/APP_NAME=$PROJECT_NAME/g" src/backend/.env
-    sed -i "s/APP_ENV=local/APP_ENV=$ENVIRONMENT/g" src/backend/.env
+    set_env "s/APP_NAME=Laravel/APP_NAME=$PROJECT_NAME/g" src/backend/.env;
+    set_env "s/APP_ENV=local/APP_ENV=$ENVIRONMENT/g" src/backend/.env;
 
     if [[ $ENVIRONMENT != "development" ]]; then
-        sed -i "s/APP_DEBUG=true/APP_DEBUG=false/g" src/backend/.env
+        set_env "s/APP_DEBUG=true/APP_DEBUG=false/g" .env;
     fi
 
     echo -e "${SUCCESS_COLOR}done${NO_COLOR}"
 
-    sed -i "s/APP_URL=http:\/\/localhost/APP_URL=http:\/\/$APP_DOMAIN/g" src/backend/.env
-    sed -i "s/DB_DATABASE=/DB_DATABASE=$MYSQL_DATABASE/g" src/backend/.env
-    sed -i "s/DB_USERNAME=/DB_USERNAME=$MYSQL_USER/g" src/backend/.env
-    sed -i "s/DB_PASSWORD=/DB_PASSWORD=$MYSQL_PASSWORD/g" src/backend/.env
-    sed -i "s/CACHE_DRIVER=file/CACHE_DRIVER=redis/g" src/backend/.env
-    sed -i "s/QUEUE_CONNECTION=sync/QUEUE_CONNECTION=redis/g" src/backend/.env
-    sed -i "s/API_URL=/API_URL=http:\/\/$APP_DOMAIN\/api\/v1/g" src/backend/.env
-    sed -i "s/STORAGE_DISK_URL=/STORAGE_DISK_URL=http:\/\/$APP_DOMAIN\/api\/storage/g" src/backend/.env
+    set_env "s/APP_URL=http:\/\/localhost/APP_URL=http:\/\/$APP_DOMAIN/g" src/backend/.env;
+    set_env "s/DB_DATABASE=/DB_DATABASE=$MYSQL_DATABASE/g" src/backend/.env;
+    set_env "s/DB_USERNAME=/DB_USERNAME=$MYSQL_USER/g" src/backend/.env;
+    set_env "s/DB_PASSWORD=/DB_PASSWORD=$MYSQL_PASSWORD/g" src/backend/.env;
+    set_env "s/CACHE_DRIVER=file/CACHE_DRIVER=redis/g" src/backend/.env;
+    set_env "s/QUEUE_CONNECTION=sync/QUEUE_CONNECTION=redis/g" src/backend/.env;
 
     # Update React .env
     echo -n "Setting up React environment variables ... "
     cp src/frontend/.env.example src/frontend/.env
-    sed -i "s/REACT_APP_SITE_TITLE=\"Sprobe Base Template\"/REACT_APP_SITE_TITLE=$PROJECT_NAME/g" src/frontend/.env
-    sed -i "s/REACT_APP_API_URL=/REACT_APP_API_URL=http:\/\/$APP_DOMAIN\/api\/v1/g" src/frontend/.env
+    set_env "s/REACT_APP_SITE_TITLE=\"Sprobe Base Template\"/REACT_APP_SITE_TITLE=$PROJECT_NAME/g" src/frontend/.env;
+    set_env "s/REACT_APP_API_URL=/REACT_APP_API_URL=http:\/\/$APP_DOMAIN\/api\/v1/g" src/frontend/.env;
     echo -e "${SUCCESS_COLOR}done${NO_COLOR}"
 
     # Build Docker Containers
@@ -117,17 +129,17 @@ if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
     CLIENT_ID=$(grep 'Client ID\:' output.txt | tail -n 1)
     CLIENT_SECRET=$(grep 'Client secret\:' output.txt | tail -n 1)
     rm output.txt
-    CLIENT_ID="$(echo ${CLIENT_ID/Client ID:/""} | tr -d '[:space:]' | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")"
-    CLIENT_SECRET="$(echo ${CLIENT_SECRET/Client secret:/""} | tr -d '[:space:]' | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")"
-    sed -i "s/API_CLIENT_ID=/API_CLIENT_ID=$CLIENT_ID/g" src/backend/.env
-    sed -i "s/API_CLIENT_SECRET=/API_CLIENT_SECRET=$CLIENT_SECRET/g" src/backend/.env
-    sed -i "s/REACT_APP_CLIENT_ID=/REACT_APP_CLIENT_ID=$CLIENT_ID/g" src/frontend/.env
-    sed -i "s/REACT_APP_CLIENT_SECRET=/REACT_APP_CLIENT_SECRET=$CLIENT_SECRET/g" src/frontend/.env
+    CLIENT_ID="$(echo ${CLIENT_ID/Client ID:/""} | tr -d '[:space:]' | perl -pe 's/\x1b\[[0-9;]*[mG]//g')"
+    CLIENT_SECRET="$(echo ${CLIENT_SECRET/Client secret:/""} | tr -d '[:space:]' | perl -pe 's/\x1b\[[0-9;]*[mG]//g')"
+    set_env "s/API_CLIENT_ID=/API_CLIENT_ID=$CLIENT_ID/g" src/backend/.env;
+    set_env "s/API_CLIENT_SECRET=/API_CLIENT_SECRET=$CLIENT_SECRET/g" src/backend/.env;
+    set_env "s/REACT_APP_CLIENT_ID=/REACT_APP_CLIENT_ID=$CLIENT_ID/g" src/frontend/.env;
+    set_env "s/REACT_APP_CLIENT_SECRET=/REACT_APP_CLIENT_SECRET=$CLIENT_SECRET/g" src/frontend/.env;
 
     # Start the containers
     docker-compose up -d
 
     # Display the results
     echo -e "\n\n${SUCCESS_COLOR}Project Setup Completed${NO_COLOR}"
-    echo -e "You can now access the app by clicking this link ${SUCCESS_COLOR}\e]8;;http://$APP_DOMAIN\ahttp://$APP_DOMAIN\e]8;;\a${NO_COLOR}"
+    echo -e "You can now access the app by clicking this link ${SUCCESS_COLOR}http://${APP_DOMAIN}${NO_COLOR}"
 fi
